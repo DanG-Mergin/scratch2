c9405a5b3db431fa89e04a356fe737d185e55f52
DanG-Mergin <dan.goldberg321@gmail.com>
2023-07-29 17:24:10 -0400

    removing commented code for more legibility

--- Components/Annotation/LabelList.tsx
+++ Components/Annotation/LabelList.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { useAnnotationService } from './AnnotationService';
-// import { Button, ButtonGroup } from '@mui/material';
 import MDBox from 'components/MDBox';
 import MDButton from 'components/MDButton';
 
